a3846dd336c37022c51e5030090e093ebdd5ffee
Task Nagashige <task.19.xx@gmail.com>
2020-07-06 20:46:57 +0900

    DBから掲示板を読み込めるようにした

--- Thread.php
+++ Thread.php
@@ -11,6 +11,7 @@ class Thread
     private $name;
     private const THREAD_FILE = 'thread.txt';
     private $dotenv;
+    private $dbh;
 
     // コンストラクタ
     public function __construct(string $name)
@@ -18,26 +19,44 @@ class Thread
         $this->name = $name;
         $this->dotenv = Dotenv::createImmutable(__DIR__);
         $this->dotenv->load();
+
+        $this->dbh = new PDO('mysql:dbname='.$_ENV['DB_NAME'].';host=127.0.0.1', $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
     }
 
     public function getList()
     {
-        return file_get_contents(self::THREAD_FILE);
+        $res = "";
+        $stmt = $this->dbh->query("SELECT * FROM `thread` WHERE `deleted_at` IS NULL ORDER BY `created_at` ASC");
+        while($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
+            $created_at = $row["created_at"];
+            $name = $row["name"];
+            $contents = $row["content"];
+            $res .= "<hr>\n";
+            $res .= "<p>投稿日時: ".date("Y/m/d H:i:s", strtotime($created_at))."</p>\n";
+            $res .= "<p>投稿者:".$name."</p>\n";
+            $res .= "<p>内容:</p>\n";
+            $res .= "<p>".nl2br($contents)."</p>\n";
+        }
+
+        return $res;
     }
 
+    /**
+     * @param string $personal_name
+     * @param string $contents
+     */
     public function post(string $personal_name, string $contents)
     {
-        $data = "<hr>\n";
-        $data = $data."<p>投稿日時: ".date("Y/m/d H:i:s")."</p>\n";
-        $data = $data."<p>投稿者:".$personal_name."</p>\n";
-        $data = $data."<p>内容:</p>\n";
-        $data = $data."<p>".nl2br($contents)."</p>\n";
-
-        file_put_contents(self::THREAD_FILE, $data, FILE_APPEND);
+        $stmt = $this->dbh->prepare("INSERT INTO `thread` (name, content) VALUES (:name, :content)");
+        $stmt->bindParam(':name', $personal_name, PDO::PARAM_STR);
+        $stmt->bindParam(':content', $contents, PDO::PARAM_STR);
+        $stmt->execute();
     }
 
     public function delete()
     {
-        file_put_contents(self::THREAD_FILE, "");
+        $sql = "UPDATE `thread` SET `deleted_at` = NOW()";
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->execute();
     }
 }
--- migrations/20200701000000_CreateThread.php
+++ migrations/20200701000000_CreateThread.php
@@ -15,6 +15,7 @@ CREATE TABLE IF NOT EXISTS `thread` (
 `name` varchar(255) NOT NULL,
 `content` varchar(255) NOT NULL,
 `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`deleted_at` DATETIME DEFAULT NULL,
 PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8;
 EOT;
